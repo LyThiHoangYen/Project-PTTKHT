461bcaabd1e99c428cb4f284a0adfe09e9044565
NgocChau0403 <21521885@gm.uit.edu.vn>
2024-05-27 23:32:21 +0700

    code back tối cn

--- src/user/ForgotPassword.java
+++ src/user/ForgotPassword.java
@@ -186,7 +186,8 @@ public class ForgotPassword extends javax.swing.JFrame {
     }//GEN-LAST:event_jLabel7MouseClicked
 
     private void btcbackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btcbackActionPerformed
-        // TODO add your handling code here:
+        new Login().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_btcbackActionPerformed
 
     /**
