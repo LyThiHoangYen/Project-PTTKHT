c65d7635d2e8570dcd0a30bcb2d7b146b7e30325
NgocChau0403 <21521885@gm.uit.edu.vn>
2024-06-02 10:53:56 +0700

    user account

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -86,4 +86,81 @@ public class UserDao {
             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    //update user data
+    public void update(int id, String username, String email, String pass, String phone,
+            String address, String seq, String ans){
+        String sql = "update staff set  sid = ?, sname = ?, semail = ?, spassword = ?, sphone = ?, saddress = ?, sques = ?, sans = ?";
+        try {
+            ps = con.prepareStatement (sql);
+            ps.setInt(1, id);
+            ps.setString(2, username);
+            ps.setString(3, email);
+            ps.setString(4, pass);
+            ps.setString(5, phone);
+            ps.setString(6, address);
+            ps.setString(7, seq);
+            ps.setString(8, ans);
+            if(ps.executeUpdate()>0){
+                JOptionPane.showMessageDialog(null, "User data successfully updated");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //delete user
+    public void delete(int id){
+        int x = JOptionPane.showConfirmDialog(null, "Are you sure to delete this account?","Delete account", JOptionPane.OK_CANCEL_OPTION,0);
+        if(x == JOptionPane.OK_OPTION){
+            try {
+                ps = con.prepareStatement("delete from staff wwhere sid = ?");
+                ps.setInt(1, id);
+                if(ps.executeUpdate() > 0){
+                    JOptionPane.showMessageDialog(null, "Account deleted");
+                }
+            } catch (SQLException ex){
+                Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE,null, ex);
+            }
+        }
+    }
+    
+    //get user value
+    public String[] getUserValue(int id){
+        String[] value = new String[8];
+        try {
+            ps = con.prepareStatement("select * from staff where sid = ?");
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                value[0] = rs.getString(1);
+                value[1] = rs.getString(2);
+                value[2] = rs.getString(3);
+                value[3] = rs.getString(4);
+                value[4] = rs.getString(5);
+                value[5] = rs.getString(6);
+                value[6] = rs.getString(7);
+                value[7] = rs.getString(8);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return value;
+    }
+    
+    //get user id
+    public int getUserId(String email){
+        int id = 0;
+        try {
+            ps = con.prepareStatement("select sid from staff where semail = ?");
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                id = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
+    }
 }
